3828e56a8ad47f1ed7e552cb42112d552728d7c0
Chamod Kalpitha <chamod.kalpitha@oxoholdings.biz>
2025-02-22 11:47:24 +0530

    refactor(dao): remove unused BillDAO, BillDAOImpl, StaffDAO, and StaffDTO classes

--- src/main/java/com/bms/dao/BillDAO.java
+++
@@ -1,9 +0,0 @@
-package com.bms.dao;
-
-import java.sql.SQLException;
-
-import com.bms.model.Bill;
-
-public interface BillDAO {
-	Bill create(Bill bill) throws SQLException;
-}
--- src/main/java/com/bms/dao/StaffDAO.java
+++
@@ -1,7 +0,0 @@
-package com.bms.dao;
-
-
-public interface StaffDAO {
-	
-
-}
